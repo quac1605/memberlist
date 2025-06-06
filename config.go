a534deb99a3a5782a1bce1e86d42c6661148e877
quac1605 <nnquang16051998@gmail.com>
2025-06-06 15:26:02 +0200

    Fix

--- config.go
+++ config.go
@@ -308,16 +308,17 @@ func DefaultLANConfig() *Config {
 		AdvertiseAddr:           "",
 		AdvertisePort:           7946,
 		ProtocolVersion:         ProtocolVersion2Compatible,
-		TCPTimeout:              10 * time.Second,       // Timeout after 10 seconds
-		IndirectChecks:          3,                      // Use 3 nodes for the indirect ping
-		RetransmitMult:          4,                      // Retransmit a message 4 * log(N+1) nodes
-		SuspicionMult:           4,                      // Suspect a node for 4 * log(N+1) * Interval
-		SuspicionMaxTimeoutMult: 6,                      // For 10k nodes this will give a max timeout of 120 seconds
-		PushPullInterval:        30 * time.Second,       // Low frequency
-		ProbeTimeout:            200 * time.Millisecond, // Reduced from 500ms - critical for RTT accuracy
-		ProbeInterval:           500 * time.Millisecond, // Reduced from 1s - more frequent probing
-		DisableTcpPings:         false,                  // TCP pings are safe, even with mixed versions
-		AwarenessMaxMultiplier:  8,                      // Probe interval backs off to 8 seconds
+		TCPTimeout:              10 * time.Second, // Timeout after 10 seconds
+		IndirectChecks:          3,                // Use 3 nodes for the indirect ping
+		RetransmitMult:          4,                // Retransmit a message 4 * log(N+1) nodes
+		SuspicionMult:           4,                // Suspect a node for 4 * log(N+1) * Interval
+		SuspicionMaxTimeoutMult: 6,                // For 10k nodes this will give a max timeout of 120 seconds
+		PushPullInterval:        30 * time.Second, // Low frequency
+		// KEY RTT OPTIMIZATION CHANGES:
+		ProbeTimeout:           200 * time.Millisecond, // Reduced from 500ms - critical for RTT accuracy
+		ProbeInterval:          500 * time.Millisecond, // Reduced from 1s - more frequent probing
+		DisableTcpPings:        true,                   // Disable TCP pings to avoid mixed RTT measurements
+		AwarenessMaxMultiplier: 4,
 
 		GossipNodes:          3,                      // Gossip to 3 nodes
 		GossipInterval:       200 * time.Millisecond, // Gossip more rapidly
